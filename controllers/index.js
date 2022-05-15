99cd6c7a8394ec59cb7cd336f9382e67e804fcc1
Christian Carnevale <cncarnevale@gmail.com>
2022-05-15 11:51:14 -0400

    revised controleer files, connection file, handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,8 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
-const Post = require('../models');
+const apiRoutes = require('./api/user-routes');
+const homeRoutes = require('./homeRoutes');
 
-// const apiRoutes = require('./api');
-const { Post, Comment } = require('../models');
-// const homeRoutes = require('./homeRoutes');
-
-router.get('/post', (req, res) => {
-    console.log('GET POST');
-});
-
-router.get('/post', async (req, res) => {
-	try {
-        console.log('GET POST');
-		const allPost = await Post.findAll();
-		const serializedPost = allPost.map((post) =>
-			post.get({ plain: true })
-		);
-		console.log(serializedPost);
-		res.render('home', { serializedPost });
-	} catch (err) {
-		res.status(404).json('');
-	}
-});
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
