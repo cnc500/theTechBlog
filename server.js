ff2b1c97b11b1e240a12dda9fc49b163b2d71405
Christian Carnevale <cncarnevale@gmail.com>
2022-05-31 10:26:21 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-const express = require('express');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-require('dotenv').config();
-const routes = require('./controllers');
-const session = require ('express-session');
+const express = require("express");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const session = require("express-session");
 const hbs = exphbs.create({});
-const path = require('path');
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelizeStore = require('connect-session-sequelize')
-(session.Store);
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {maxAge: 200000},
-    resave: false,
-    rolling: true,
-    saveUninitialized: true,
-    store: new sequelizeStore({
-        db: sequelize
-    }) 
-}
-app.use(session(sess))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 200000 },
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
 // Force is false so program does not drop database each time
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log('Testing 123');
-    });
+  app.listen(PORT, () => {
+    console.log("Testing 123");
+  });
 });
